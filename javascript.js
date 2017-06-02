b66f9267f15d9bf92ce264bd0c222f5c117261d2
jordnkr <jordnkr408@gmail.com>
2017-06-01 23:37:47 -0500

    adding example framework

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function $(selector) { return document.querySelector(selector);  }
+function $$(selector) { return document.querySelectorAll(selector); }
+
+/*
+
+$("#someid"); //fetch the element with the id ‘someid’
+
+function printHello() {
+   console.log("Hello!")
+}
+$$("button").forEach(button => button.addEventListener('click', printHello));
+
+*/
